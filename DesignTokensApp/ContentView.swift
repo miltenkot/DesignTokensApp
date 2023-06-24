fd1a41d472ef20d214ae6234a6c1f43f4f0752e0
miltenkot <miltenkot@gmail.com>
2023-06-24 08:16:05 +0200

    Initial commit

---
+++ DesignTokensApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DesignTokensApp
+//
+//  Created by Bartlomiej Lanczyk on 24/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DesignTokensApp/DesignTokensAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignTokensApp.swift
+//  DesignTokensApp
+//
+//  Created by Bartlomiej Lanczyk on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct DesignTokensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
